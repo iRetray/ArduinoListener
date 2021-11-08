8345ba680c1662ca5ebc81359a90d36e1028e05f
Julian Camilo <julian20141016@gmail.com>
2021-11-08 17:54:44 -0500

    Creada integracion de prueba con Realtime Database

--- src/index.js
+++ src/index.js
@@ -6,16 +6,26 @@ import ArduinoListener from "./ArduinoListener";
 import {
   FirebaseAppProvider,
   FirestoreProvider,
+  DatabaseProvider,
   useFirebaseApp,
 } from "reactfire";
 import { getFirestore } from "firebase/firestore";
+import { getDatabase } from "firebase/database";
+
+import ComponentWithRealtime from "./ComponentWithRealtime";
+
 import firebaseConfig from "./firebaseConfig";
 
 const AppWithFirebase = () => {
   const firestoreInstance = getFirestore(useFirebaseApp());
+  const databaseInstance = getDatabase(useFirebaseApp());
+
   return (
     <FirestoreProvider sdk={firestoreInstance}>
-      <ArduinoListener />
+      <DatabaseProvider sdk={databaseInstance}>
+        <ArduinoListener />
+        <ComponentWithRealtime />
+      </DatabaseProvider>
     </FirestoreProvider>
   );
 };
